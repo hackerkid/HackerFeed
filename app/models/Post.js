4202b760e8d224601d8e59c4510228b9278ce89b
Vishnu Ks <yo@vishnuks.com>
2015-06-19 23:06:53 +0530

    first commit

---
+++ app/models/Post.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Post', {
+	title:  String,
+  	author: String,
+  	url: String,
+  	blogId: String,
+  	body:   String,
+  	comments: [{ body: String, date: Date }],
+  	date: { type: Date, default: Date.now },
+  	hidden: Boolean,
+  	meta: {
+    upVotes: Number,
+    downVotes:  Number
+    }
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+// set up ======================================================================
+var express = require('express');
+var app = express(); // create our app w/ express
+var mongoose = require('mongoose'); // mongoose for mongodb
+var port = process.env.PORT || 8080; // set the port
+var database = require('./config/database'); // load the database config
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var Post = require("./app/models/Post.js")
+
+// configuration ===============================================================
+mongoose.connect(database.url); // connect to mongoDB database on modulus.io
+
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
+
+Post.count({
+    'title': 'hackers'
+}, function(err, count) {
+    
+    if (err) {
+        return handleError(err);
+    } else {
+		console.log("There are" +count);	
+    }
+})
